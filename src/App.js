603a8e9c8924cb38aecaee5249876f6768083a6c
Kajalben <kajalsavani191@gmail.com>
2021-02-01 17:40:03 +0100

    project Structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+import News from './News';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <News />
+      <Footer />
+
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/News.js
@@ -0,0 +1,9 @@
+const News = () =>{
+    return(
+        <>
+        <h1>News</h1>
+        </>
+    );
+}
+
+export default News;
