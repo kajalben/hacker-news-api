92ec601437e292f851708494322ca048e1f60122
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-02-02 17:11:37 +0100

    img folders done

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,24 @@
+import "./footerstyle.css";
+import fb from './img/fb.png';
+import ig from './img/ig.png';
+import lin from './img/in.png';
+import tw from './img/tw.png';
+import ytu from './img/ytu.png';
+
+
 const Footer = () =>{
     return(
-        <>
-        <h1>Footer</h1>
-        </>
+        <footer className="footer">
+        <h1>Follow Us</h1>
+        <ul className="socials">
+            <li><img src={fb}/></li>
+            <li><img src={ig}/></li>
+            <li><img src={lin}/></li>
+            <li><img src={tw}/></li>
+            <li><img src={ytu}/></li>
+        </ul>
+        
+        </footer>
     );
 }
 
